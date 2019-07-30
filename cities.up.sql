a0c8114369d1c32f728c4fe08b2743bd598ac2ea
sveri <sveri80@gmail.com>
2019-07-30 12:44:00 +0200

    create lower_name column

--- cities.up.sql
+++ cities.up.sql
@@ -1,11 +1,20 @@
+DROP TABLE IF EXISTS cities;
+
+
 CREATE TABLE cities (
 id bigserial NOT NULL PRIMARY KEY,
 nodes_id bigint,
 name text,
+lower_name text,
 is_in text,
 country text,
 continent text
 );
 
 ALTER TABLE cities OWNER TO osmcities;
+
+
+CREATE INDEX cities_name_idx ON cities(name);
+
+CREATE INDEX cities_lowername_idx ON cities(lower_name);
 
--- src/main/clj/main.clj
+++ src/main/clj/main.clj
@@ -1,5 +1,6 @@
 (ns main
-  (:require [clojure.java.jdbc :as j])
+  (:require [clojure.java.jdbc :as j]
+            [clojure.string :as str])
   (:gen-class))
 
 
@@ -9,8 +10,15 @@
   (j/query db ["select id as nodes_id, tags -> 'name' AS name, tags -> 'is_in' AS is_in, tags -> 'is_in:country' as
                 country, tags -> 'is_in:continent' as continent from nodes"]))
 
+(defn get-lower-name [node]
+  (let [name (get node :name "")]
+    (if (nil? name)
+      nil
+      (str/lower-case name))))
+
 (defn convert-osm-to-cities-table [db]
   (let [nodes (get-all-nodes db)
+        nodes (mapv #(assoc % :lower_name (get-lower-name %)) nodes)
         _ (j/insert-multi! db :cities nodes)]))
 
 
